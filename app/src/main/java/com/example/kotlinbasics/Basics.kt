7840d89a7415da0e36b46589f6a1c0cbd640fe10
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-07-11 15:18:11 +0200

    First things with Kotlin

---
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -0,0 +1,25 @@
+package com.example.kotlinbasics
+
+fun main(){
+
+     var myAge : Byte = 35
+     println(myAge)
+
+     myAge = 36
+
+     println("Hello Giacomo")
+     println(myAge)
+
+     //E' di default Double
+     //val pi = 3.14
+     //println(pi)
+
+     //Se lo voglio double
+     var pi : Float = 3.14f
+     println(pi)
+     //Cambio il valore
+     pi = 3.1415926535f
+     //Lo arrattonda perché float non basta
+     println(pi)
+
+}
